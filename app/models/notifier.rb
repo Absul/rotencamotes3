fd43f4482f0f8c7adae45e62e2e8bf009c17803f
Daso <daalsuol@gmail.com>
2013-02-22 16:48:57 -0500

    yolo2

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,14 +3,14 @@ class Notifier < ActionMailer::Base
   # default :from => "sender_email_id"
 
   def comment_posted(comment,post,user,author)
-    subject    "[Votencamotes] Alguien se encamotó con tu camoteada"
+    subject    "[Votencamotes] Alguien se encamoto con tu camoteada"
     from        "camote@votencamotes.com"
     recipients  author.email
     content_type "text/html"
     body(:post => post, :user => user, :author => author, :comment => comment)
   end
   def recomendation_posted(recomendation)
-    subject "[Votencamotes] Un amigo te recomienda una película"
+    subject "[Votencamotes] Un amigo te recomienda una pelicula"
     from "camote@votencamotes.com"
     recipients recomendation.email
     content_type "text/html"
